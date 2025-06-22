c56d08c0ca6aec2d08d7458785fbd26048dc5a85
Bụt <158729654+zinvnreview@users.noreply.github.com>
2025-06-23 01:35:44 +0700

    fix update

--- updater.php
+++ updater.php
@@ -2,15 +2,14 @@
 // File: updater.php
 
 if ( ! class_exists( 'Puc_v5_Factory' ) ) {
-    require_once plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
+    require_once plugin_dir_path( __DIR__ ) . 'plugin-update-checker/plugin-update-checker.php';
 }
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $updateChecker = PucFactory::buildUpdateChecker(
-    'https://github.com/zinvnreview/woo_shipping_plugin', 
-    __FILE__, 
+    'https://github.com/zinvnreview/woo_shipping_plugin',
+    __DIR__ . '/shipping_woo.php',
     'woo_shipping_plugin'
 );
-
 $updateChecker->setBranch('main');
